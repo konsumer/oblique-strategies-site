ff9deda8e2f1dc0b6f49dc24585b4748e3d59b04
David Konsumer <konsumer@jetboystudio.com>
2020-06-30 17:10:48 -0700

    improve look, allow selecting edition

--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import React, { useState } from 'react'
 import { render } from 'react-dom'
 import { getInspirationCard } from '@nessiesnippets/oblique-strategies'
 
-const App = ({ edition = 3 }) => {
+const App = () => {
+  const [edition, setEdition] = useState(3)
   const [text, setText] = useState(getInspirationCard(edition))
+
+  const updateEdition = e => () => {
+    setEdition(e)
+    setText(getInspirationCard(e))
+  }
+
   return (
-    <div className='card' onClick={e => setText(getInspirationCard(edition))}>
-      <div className='text' dangerouslySetInnerHTML={{ __html: text.replace(/\n/g, '<br />') }} />
+    <div className='app'>
+      <div className='edition'>
+        Edition:
+        <label><input type='radio' name='edition' onChange={updateEdition(1)} checked={edition === 1} />1</label>
+        <label><input type='radio' name='edition' onChange={updateEdition(2)} checked={edition === 2} />2</label>
+        <label><input type='radio' name='edition' onChange={updateEdition(3)} checked={edition === 3} />3</label>
+        <label><input type='radio' name='edition' onChange={updateEdition(4)} checked={edition === 4} />4</label>
+      </div>
+      <div className='card' onClick={() => setText(getInspirationCard(edition))}>
+        <div className='text' dangerouslySetInnerHTML={{ __html: text.replace(/\n/g, '<br />') }} />
+      </div>
+      {/* <button onClick={() => setText(getInspirationCard(edition))}>ANOTHER</button> */}
+      <small>Click card for more</small>
     </div>
   )
 }
